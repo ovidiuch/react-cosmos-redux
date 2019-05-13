3f08b717228e56acbaf94d0d5c20f06683ba2a46
Ovidiu Cherecheș <skidding@gmail.com>
2019-05-13 23:34:23 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -44,15 +44,16 @@ export function ReduxMock<ReduxState extends object>({
   // Synchronize fixture state with local Redux state
   // const { changedAt, storeState } = reduxContext;
   const { changedAt } = reduxContext;
+  const storeState = store.getState();
   React.useEffect(() => {
     setFixtureState(fixtureState => ({
       ...fixtureState,
       redux: {
         changedAt,
-        state: store.getState()
+        state: storeState
       }
     }));
-  }, [changedAt, store, setFixtureState]);
+  }, [changedAt, storeState, setFixtureState]);
 
   // Override local Redux state when fixture changed by other client
   React.useEffect(() => {
